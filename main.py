437d392fc5e19235d34459d84b35a071f5503903
dargem <genericz145@gmail.com>
2026-03-18 13:32:25 +1100

    more matchmaking

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ df = pd.read_csv("match_players.csv")
 # Drop unnecessary columns and nan
 df = df.drop(columns=["faction", "new_uncertainty", "left_after", "party_id", "user_id"])
 df = df.dropna()
-df.head()
 
 # %%
 # Create a new won column, 1 if the player won 0 otherwise
@@ -18,5 +17,18 @@ df.drop(columns=["new_skill", "team_id"])
 # %%
 # Group by match_id and won (teams effectively)
 df = df.groupby(["match_id", "won"])[["old_skill", "old_uncertainty"]].agg(list).reset_index()
-df.head()
+df = df.rename(columns={"old_skill": "skill", "old_uncertainty": "uncertainty"})
+
 # %%
+# merge winners and losers together
+out = (
+    df.set_index(["match_id", "won"])[["skill", "uncertainty"]]
+      .unstack("won")
+)
+
+out.columns = [
+    f"{'win' if won == 1 else 'loss'}_{col}"
+    for col, won in out.columns
+]
+
+out = out.reset_index()
